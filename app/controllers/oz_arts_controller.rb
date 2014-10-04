cd1ff11a04514ed8dadb7647ff9bb5904d1c6c15
christopher floess <skeptikos@gmail.com>
2014-10-04 10:35:13 +0200

    render different variants for different devices

--- app/controllers/oz_arts_controller.rb
+++ app/controllers/oz_arts_controller.rb
@@ -1,6 +1,18 @@
 class OzArtsController < ApplicationController
   before_action :detect_device_type
 
+  def index
+    respond_to do |format|
+      format.html do |html|
+        html.phone do
+          @oz_art = OzArt.new
+        end
+
+        @oz_arts = OzArt.all
+      end
+    end
+  end
+
   def new
     @oz_art = OzArt.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'oz_arts#new'
-  resources :oz_arts, only: [:new, :create, :show]
+  root 'oz_arts#index'
+  resources :oz_arts, only: [:new, :create, :show, :index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
